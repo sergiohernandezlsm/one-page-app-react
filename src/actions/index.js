7a15b24c101cf755cb4df40bc49a8ce50c7ad5ff
Sergio  Hernandez <sergiohernandez@sergios-mbp.lsm.local>
2019-10-29 15:07:22 +0000

    redux actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,25 @@
-import * as UserActionTypes from './types/user';
+import { GET_USERS, GET_USER } from './types';
+import axios from 'axios';
 
 export const getUser = (first_name , last_name) => {
   return {
-    type: UserActionTypes.GET_USER,
+    type: GET_USER,
     first_name,
     last_name
   };
 };
+
+export const getUsers = () => {
+
+  return (dispatch) => {
+    axios.get(`http://localhost:3000/api/users`)
+    .then(res => {
+      const users = res.data;
+      dispatch({
+        type: GET_USERS,
+        users
+      })
+    })
+  }
+
+}
