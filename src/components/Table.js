dcfaf73e69ca034c5975c0292b618c90e6d4eb3e
Sergio  Hernandez <sergiohernandez@sergios-mbp.lsm.local>
2019-10-29 10:37:54 +0000

    form table

---
+++ src/components/Table.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Row, Col, Table, ButtonToolbar, Button } from 'react-bootstrap';
+import AddButton from './AddButton'; 
+import FormRow from './FormRow';
+
+const DisplayTable = ({users}) => {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <FormRow users={users} />
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+      <AddButton />
+    </div>
+  )
+}
+
+export default DisplayTable;
